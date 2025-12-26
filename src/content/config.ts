e9c9e67fa910b5384995e817b5810891fa9d461d
Uhshoi1206 <tu@c2.hu>
2025-12-26 23:14:09 +0700

    xucdao

--- src/content/config.ts
+++ src/content/config.ts
@@ -165,7 +165,7 @@ const productsCollection = defineCollection({
     cabinFeatures: z.array(z.string()).optional(),
 
     // Vehicle Types
-    boxType: z.enum(['đông-lạnh', 'bảo-ôn', 'kín', 'bạt', 'lửng', 'bồn-xi-téc', 'cánh-dơi', 'ben', 'trộn-bê-tông', 'bơm-bê-tông', 'chở-gia-súc', 'chuyên-dùng', 'xe-tải-gắn-cẩu']).optional(),
+    boxType: z.enum(['đông-lạnh', 'bảo-ôn', 'kín', 'bạt', 'lửng', 'bồn-xi-téc', 'cánh-dơi', 'ben', 'trộn-bê-tông', 'bơm-bê-tông', 'chở-gia-súc', 'chuyên-dùng', 'xe-tải-gắn-cẩu', 'máy-xúc-đào']).optional(),
     craneType: z.enum(['cẩu-rời', 'cẩu-gắn-xe']).optional(),
     trailerType: z.enum(['ben', 'sàn', 'sàn-rút', 'lùn', 'cổ-cò', 'xương', 'lửng', 'lồng', 'rào', 'xi-téc', 'bồn-xi-măng', 'bồn-sắt', 'bồn-bột-mì', 'đông-lạnh', 'hút-chất-thải', 'bồn-nh3', 'bồn-ni-tơ', 'bồn-hóa-chất', 'bồn-bột-pvc', 'bồn-nhựa-đường', 'bồn-thức-ăn', 'bồn-lpg', 'bồn-methanol', 'bồn-hạt-nhựa', 'bồn-co2', 'bồn-ethylene-glycol']).optional(),
 
